010f53614d105e3ebdfcc43d853ec773107143fe
supuna <92195424+supuna28@users.noreply.github.com>
2021-10-13 21:05:29 +0530

    Update update2.js

--- plugins/update2.js
+++ plugins/update2.js
@@ -4,7 +4,7 @@ const { promisify } = require('util')
 const { join } = require('path')
 
 let confirmation = {}
-let repository = 'ariffb25/stikerinbot'
+let repository = 'supuna28/buttonbot'
 let branch = 'main'
 
 async function handler(m, { text }) {
