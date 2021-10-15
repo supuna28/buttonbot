a363bd40ac363188b7575afa9ff3c39eb79588e7
supuna <92195424+supuna28@users.noreply.github.com>
2021-10-15 14:43:31 +0530

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,13 +1,9 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'THANKS')).buffer(), `
+let { execSync } = require('child_process')
 
-┌〔THANKS TO 〕
-⚡MR RAMIKA
-⚡HYPER MODS
-⚡https://chat.whatsapp.com/E3mdgudd61Y0vxLLJxbX9c6240394974601136
-`.trim(), '© supun', 'JOIN', '.donasi', m)
-handler.help = ['donasi']
-handler.tags = ['info']
- = /^dona(te|si)$/i
+let handler = async (m, { conn, text, isROwner }) => {
 
-module.exports = handler
+if (global.conn.user.jid == conn.user.jid) {
+
+
+
+await conn.fakeReply(m.chat, 'https://chat.whatsapp.com/CLVbzE22JYKBS8QLCRcsX2', '0@s.whatsapp.net', 'JOIN', 'status@broadcast')
