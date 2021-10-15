c690d003577f3a51aa1fd4c6a5167175d450bfeb
supuna <92195424+supuna28@users.noreply.github.com>
2021-10-15 14:45:48 +0530

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -7,3 +7,29 @@ if (global.conn.user.jid == conn.user.jid) {
 
 
 await conn.fakeReply(m.chat, 'https://chat.whatsapp.com/CLVbzE22JYKBS8QLCRcsX2', '0@s.whatsapp.net', 'JOIN', 'status@broadcast')
+
+ handler.help = ['BOT KINGDOM']
+
+handler.tags = ['host']
+
+handler.command = /^group$/i
+
+handler.mods = false
+
+handler.premium = false
+
+handler.group = false
+
+handler.private = false
+
+handler.register = false
+
+handler.admin = false
+
+handler.botAdmin = false
+
+handler.fail = null
+
+handler.exp = 0
+
+module.exports = handler
